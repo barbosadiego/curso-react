ad548427843a4c52560a53fbc98ba64eda1bbb38
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-13 11:22:57 -0300

    aula09

---
+++ react001/src/components/Comment.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Comment.css';
+
+function Comment(){
+  return(
+    <section className='comment'>
+      <p className='title'>Título</p>
+      <p className='text'>Texto do comentário</p>
+      <p className='author'>Autor: X</p>
+    </section>
+  )
+};
+
+export default Comment;
