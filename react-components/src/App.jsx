6fda3db9709c3a2ef6242ffb8c27868980b33c64
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-13 14:40:57 -0300

    aula 12

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -5,13 +5,16 @@ import './App.css';
 
 // Componentes
 import ComponenteUm from './components/ComponenteUm';
+import ComponenteDois from './components/ComponenteDois';
+import ComponenteTres from './components/ComponenteTres';
 
 function App(){
   return(
     <>
       <h1>App</h1>
       <ComponenteUm />
-      <ComponenteUm />
+      <ComponenteDois />
+      <ComponenteTres title='teste'/>
     </>
   )
 };
---
+++ react-components/src/components/ComponenteDois.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComponenteDois = () => {
+  return(
+    <div className='component'>
+      <p>Título: Componente Dois</p>
+    </div>
+  )
+};
+
+export default ComponenteDois;
