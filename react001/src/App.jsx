74082ba10766c89f080c497b2f6caf9b8961095e
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-13 11:38:00 -0300

    aula10

--- react001/src/App.jsx
+++ react001/src/App.jsx
@@ -5,9 +5,9 @@ function App(){
   return(
     <>
       <h1>Comentários</h1>
-      <Comment />
-      <Comment />
-      <Comment />
+      <Comment title='Comentário Um' author='Diego Barbosa'/>
+      <Comment title='Comentário Dois' author='João Ribeiro'/>
+      <Comment title='Comentário Três' author='Mariazinha'/>
     </>
   )
 };
--- react001/src/components/Comment.jsx
+++ react001/src/components/Comment.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Comment.css';
 
-function Comment(){
+function Comment(props){
+
+  function Maiúsculas(texto){
+    return texto.toUpperCase();
+  }
+
   return(
     <section className='comment'>
-      <p className='title'>Título</p>
+      <p className='title'>{Maiúsculas(props.title)}</p>
       <p className='text'>Texto do comentário</p>
-      <p className='author'>Autor: X</p>
+      <p className='author'>Autor: {props.author}</p>
     </section>
   )
 };
