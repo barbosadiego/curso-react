daccf29c8379e850c8889ff9bc9c2ebd57f75ca2
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-13 14:26:45 -0300

    aula 11

---
+++ react-components/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// CSS
+import './App.css';
+
+// Componentes
+import ComponenteUm from './components/ComponenteUm';
+
+function App(){
+  return(
+    <>
+      <h1>App</h1>
+      <ComponenteUm />
+      <ComponenteUm />
+    </>
+  )
+};
+
+export default App;
