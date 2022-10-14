36fbd6b0f8066397d3df4e9e483d875f4aa1f778
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-14 11:09:37 -0300

    aula 26

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -2,22 +2,23 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+
   const [valor, setValor] = useState(0);
 
-  function aumentar() {
-    setValor(valor + 1);
-  }
+  function incremento(){
+    setValor(valor_antigo => valor_antigo + 1);
+  };
 
-  function diminuir(){
-    setValor(valor - 1);
-  }
+  function decremento(){
+    setValor(valor_antigo => valor_antigo - 1);
+  };
 
   return (
     <>
-      <h1>React Hooks</h1>
-      <h3>Valor: {valor}</h3>
-      <button onClick={aumentar}>Aumentar</button>
-      <button onClick={diminuir}>Diminuir</button>
+      <h1>React Hooks - useState</h1>
+      <p>{valor}</p>
+      <button onClick={incremento}>Aumentar</button>
+      <button onClick={decremento}>Diminuir</button>
     </>
   );
 };
