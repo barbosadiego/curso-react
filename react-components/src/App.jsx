6d5984bb8dffa3fa7d7dd0782835e69b6e2d01cc
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-14 10:18:47 -0300

    aula 24

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 const App = () => {
+  const [valor, setValor] = useState(0);
+
+  function incrementar() {
+    setValor(valor + 1);
+  }
+
   return (
     <>
       <h1>React Hooks</h1>
+      <h3>Valor: {valor}</h3>
+      <button onClick={incrementar}>Incrementar</button>
     </>
-  )
+  );
 };
 
 export default App;
