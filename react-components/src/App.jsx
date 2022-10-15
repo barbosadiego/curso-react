22d2886588e932d372dbd5e33c3d2ccaa11b8d28
diegobarbosa <diego.gutemberg@hotmail.com>
2022-10-15 16:36:00 -0300

    aula 49

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -1,32 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const App = () => {
-  const [valor, setValor] = useState(1);
+  //states
+  const [contato, setContato] = useState({ nome: '', telefone: '' });
+  const [listaContatos, setListaContatos] = useState([]);
 
-  const multiplicar = () => {
-    if(isNaN(valor)) return [];
-    let produtos = [];
-    for (let m = 1; m <= 10; m++) {
-      produtos.push(m * valor);
-    }
-    return produtos;
-  };
+  //ref
+  const nome = useRef();
+
+  //métodos
+  function definirNome(e) {
+    setContato({ ...contato, nome: e.target.value });
+  }
+
+  function definirTelefone(e) {
+    setContato({ ...contato, telefone: e.target.value });
+  }
+
+  function adicionarContato(){
+    setListaContatos(old => [...old, contato]);
+    setContato(old => ({nome: '', telefone: ''}));
+    nome.current.focus();
+  }
 
   return (
-    <div>
-      <h1>React - Tabuada</h1>
+    <>
+      <h1>Minha lista de contatos</h1>
       <hr />
-      <input
-        type="number"
-        value={valor}
-        onChange={(e) => setValor(parseInt(e.target.value))}
-      />
-      {multiplicar().map((item, index) => (
-        <p key={index}>
-          {valor} X {index + 1} = {item}
-        </p>
-      ))}
-    </div>
+      <div>
+        <input ref={nome} type="text" onChange={definirNome} value={contato.nome} />
+      </div>
+      <div>
+        <input type="text" onChange={definirTelefone} value={contato.telefone} />
+      </div>
+      <button onClick={adicionarContato}>Adicionar Contato</button>
+      <hr />
+      {listaContatos.length > 0 && <h3>Meus contatos:</h3>}
+      {listaContatos.map((item, index) => <p key={index}>{item.nome} {item.telefone}</p>)}
+    </>
   );
 };
 
